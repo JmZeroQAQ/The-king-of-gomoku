4abf6e245632fb61162873aaf142c127495f4226
Yu Xian <1932699628@qq.com>
2023-07-18 17:34:30 +0800

    导入 Element-Plus

--- web/src/main.js
+++ web/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import '@/assets/reset.scss'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router/index'; //导入路由
 import { createPinia } from 'pinia'
@@ -9,5 +11,7 @@ app.use(router);
 
 const pinia = createPinia();
 app.use(pinia);
+
+app.use(ElementPlus);
 
 app.mount('#app');
